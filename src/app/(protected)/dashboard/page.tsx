7ba718915ca59774a683f53fe43a3c872660dad8
SamOluwadamilare300 <damilarea71@gmail.com>
2025-02-08 15:53:52 +0100

    Updated dashbaord

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,7 +1,6 @@
-import React from 'react'
 import { onBoardUser } from '@/actions/user'
 import { redirect } from 'next/navigation'
-
+import React from 'react'
 
 type Props = {}
 
@@ -16,7 +15,7 @@ const Page = async (props: Props) => {
   if (user.status === 200 || user.status === 201) {
     const firstname = user.data?.firstname ?? '' 
     const lastname = user.data?.lastname ?? ''
-    return redirect(`/dashboard/`)
+    return redirect(`/dashboard/${firstname}${lastname}`)
   }
 
   return redirect('/sign-in')
